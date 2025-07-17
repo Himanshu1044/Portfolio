0672c015227ec829b7141febef3b9a8e9bca152e
Himanshu1044 <swamihimanshu78@gmail.com>
2025-07-17 23:12:37 +0530

    Runs well locally

--- index.js
+++ index.js
@@ -1,53 +1,45 @@
-import express from 'express';
-import bodyparser from 'body-parser';
-import pg from 'pg';
+import express from 'express'
+import bodyparser from 'body-parser'
+import pg from 'pg'
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 const db = new pg.Client({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT),
-  ssl: {
-    rejectUnauthorized: false, // required for Render
-  },
 });
 
-db.connect()
-  .then(() => console.log('Connected to PostgreSQL'))
-  .catch((err) => console.error('DB connection error:', err.stack));
+db.connect();
 
 const app = express();
 const port = 3000;
-
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-  res.render('index.ejs');
-});
+    res.render('index.ejs')
+})
 
 app.post("/submit", async (req, res) => {
-  try {
-    const name = req.body.name;
-    const email = req.body.email;
-    const message = req.body.message;
+    try {
+        const name = req.body.name;
+        const email = req.body.email;
+        const message = req.body.message;
 
-    await db.query(
-      'INSERT INTO portfoliodata (name, email, message) VALUES ($1, $2, $3)',
-      [name, email, message]
-    );
+        const result = await db.query(
+            'INSERT INTO portfoliodata (name, email, message) VALUES ($1, $2, $3)',
+            [name, email, message]
+        );
 
-    res.status(201).redirect('/');
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Something went wrong" });
-  }
+        res.status(201).redirect('/');
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Something went wrong" });
+    }
 });
-
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+    console.log(`Server is running at http://localhost:${port}`);
+})

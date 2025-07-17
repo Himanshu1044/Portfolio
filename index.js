2091fdd9ee6a7ce6f1116b1858de3a03cd65cb7f
Himanshu1044 <swamihimanshu78@gmail.com>
2025-07-17 21:37:43 +0530

    Fix DB_NAME to DB_DATABASE

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const db = new pg.Client({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT),
   ssl: {
